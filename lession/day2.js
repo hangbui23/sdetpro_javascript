0b881dc6ccd9dd30b98ee44faf70b605d4494d12
Hang Bui <buihang230491@gmail.com>
2025-06-26 22:14:10 +0700

    commit file

---
+++ lession/day2.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+let isHeOnTime = true;
+let myName ="Teo";
+
+if(isHeOnTime) {
+    console.log("He is on time");
+    let myName = "Ti";
+}else {
+    console.log("He is late");
+    let myName = "Tin";
+}
+
+console.log(myName); // Output: Teo
