95acbcb74e5dc760cf40f251ea7675434462e739
Hang Bui <buihang230491@gmail.com>
2025-08-05 21:59:17 +0700

    update lab4

--- lession/Lab4/Program.js
+++ lession/Lab4/Program.js
@@ -12,19 +12,35 @@ bankAccount2.accountNumber = '5689321';
 
 let bankAccounts = [bankAccount1, bankAccount2]
 
+const MAX_INVALID_AMOUNT_COUNT = 3;
+const MAX_INSUFFICIENT_BALANCE_COUNT = 3;
+
 function findAccountNumber(accountNumner) {
     return bankAccounts.find(acc => acc.accountNumber === accountNumner);
 }
 
-function updateBalance(account, amount) {
+function updateBalance(account) {
+    let invalidAmountCount = 0;
+    let insufficientBalanceCount = 0;
+    do {
+    const amount = Number(readLine.question('Input your amount: '));
     if (Number.isNaN(amount) || amount <= 0) {
         console.log('Invalid amount. Please enter a valid number greater than 0.');
+        invalidAmountCount++;
     } else if (account.balance >= amount) {
         account.balance -= amount;
         console.log(`Balance updated successfully. New balance is ${account.balance}`);
+        let answer = readLine.question('Do you want to withdraw more? (yes/no): ');
+        if(answer.toLowerCase() === 'yes') {
+            continue; // Continue to allow more withdrawals
+        }
+        break; // Exit the loop if the user does not want to withdraw more
     } else {
         console.log('Insufficient balance. Please enter a valid amount.');
-    }
+        insufficientBalanceCount++;
+    }    
+    } while (invalidAmountCount < MAX_INVALID_AMOUNT_COUNT && insufficientBalanceCount < MAX_INSUFFICIENT_BALANCE_COUNT);
+    
 }
 
 function displayAccount(account) {
@@ -60,8 +76,7 @@ while (choice !== 0) {
                     console.log(`Account ${accountNumber} not found!`);
                 }
                 else {
-                    const amount = Number(readLine.question('Input your amount: '));
-                    updateBalance(account, amount);
+                    updateBalance(account);
                 }
                 break;
             default:
